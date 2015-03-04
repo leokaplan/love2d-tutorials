c6de276e01a65936d17d1a42b929d6977dd8078e
Leonardo Kaplan <leokaplan@users.noreply.github.com>
2015-03-04 16:27:59 -0300

    Update 3.lua

--- falling/3.lua
+++ falling/3.lua
@@ -1,10 +1,19 @@
+--[[
+    Yes, it is the same as the last one.
+    But this time we are using a variable to represent our position on the x axis.
+    In the next lesson we will start to move things!
+--]]
 function love.load()
-      x = 100 
+    -- see how a variable is declared? just choose a name and a initial value
+    -- what would happen if you set this to other values? 
+    -- can you make a variable for y too?
+    x = 100 
 end
 function love.update(dt)
     
 end
 function love.draw()
     love.graphics.setColor(255,0,0,255)
+    --here we are using our variable
     love.graphics.rectangle("fill", x, 100, 50, 50)
 end
