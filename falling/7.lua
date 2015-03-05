07e29b860eb70f1172f7b4134b0fc613b346561c
Leonardo Kaplan <leokaplan@users.noreply.github.com>
2015-03-05 16:44:21 -0300

    Update 7.lua

--- falling/7.lua
+++ falling/7.lua
@@ -2,6 +2,9 @@
     Once again, everything changed.
     Now, for the first time, you were invited to affect our simulated world with your free will.
     This lesson isn't very interesting, it's just to you see it working.
+    But one thing that is bothering me is that the square doesn't stop and it starts moving by itself.
+    It is not a great avatar if it has some kind of free will.
+    Can you change the code so that the square moves only when the user presses 'a' and 'd'?
 --]]
 
 
