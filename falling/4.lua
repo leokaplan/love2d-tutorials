4b375ce48226c0e7f46b5cfff1208956ea72c75f
Leonardo Kaplan <leokaplan@users.noreply.github.com>
2015-03-05 14:22:16 -0300

    Update 4.lua

--- falling/4.lua
+++ falling/4.lua
@@ -2,7 +2,11 @@
     That's right! Off we go! We are moving!
     Note the update function.
     We are increasing x by 1 for every call of update, but what does that mean?
-        
+        What we are doing here is moving the square by 1 pixel for every frame.
+        It is the same as increasing x in the draw function (try it!)
+    What would happen if we increase the y instead of x? and the width? 
+    can you scale the square over time, keeping the square shape?
+    what would happen if you had a slower/faster pc and a different framerate? 
 --]]
 
 function love.load()
